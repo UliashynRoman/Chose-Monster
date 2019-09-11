9e5bf173f5cf61f40e06290b375493f52a48bc92
Roman <romanuliashyn@gmail.com>
2019-09-11 12:57:58 +0300

    Updates

---
+++ precache-manifest.c540d457d1a6b4d24e2b7dbc11d82045.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38fe0891deba8ef2456c0fa8a3ea3237",
+    "url": "/react-projects/index.html"
+  },
+  {
+    "revision": "7039cc6cef3f49864273",
+    "url": "/react-projects/static/css/main.3044b207.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/react-projects/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "7039cc6cef3f49864273",
+    "url": "/react-projects/static/js/main.f7ef61c6.chunk.js"
+  },
+  {
+    "revision": "859f20094bd6d4e0b26d",
+    "url": "/react-projects/static/js/runtime~main.55f7e485.js"
+  }
+]);
